9731a511ad9b318e086fe8fdd99455aa29fc21ff
David Gago <david.gago@deimos.com.pt>
2020-03-27 20:25:00 +0000

    initial commit

---
+++ src/main/kotlin/com/gago/david/myland/MyLandBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.gago.david.myland
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MyLandBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<MyLandBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/gago/david/myland/MyLandBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gago.david.myland
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MyLandBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
